2e7578d5c6b5081e10e86c79980b059737602dba
Victor Grau Serrat <lacabra@users.noreply.github.com>
2018-06-28 12:54:58 -0500

    BLD: reconfiguring ntw for containers to interact

--- launch_network.bash
+++ launch_network.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker-compose up -d
-echo 'Waiting for Truffle to launch...'
+docker-compose up &
+echo 'Waiting for enigma-contract to launch...'
 sleep 5
 echo "Deploying contracts on testnet..."
 docker-compose exec contract bash -c "rm -rf build"
-docker-compose exec contract bash -c "~/darq-truffle migrate --reset"
+docker-compose exec contract bash -c "~/darq-truffle migrate --reset --network ganache"
 echo "Starting Surface listener..."
 docker-compose exec surface bash -c "python -m pytest surface/src/tests/test_listener.py" &
 echo "Registering worker and creating task..."
