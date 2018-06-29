161a0954b77c5f5913c801ea15ed5973521ae5b4
Victor Grau Serrat <lacabra@users.noreply.github.com>
2018-06-28 23:24:55 -0500

    MAINT: adjusting network params

--- launch_network_terminals.bash
+++ launch_network_terminals.bash
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-docker-compose up -d
+# Alternatively: `docker-compose up -d`, but by launching it in the background, 
+# we capture the output on the current terminal & we can see the list of 
+# accounts that ganache creates (different every time)
+docker-compose up &
 xterm -geometry 90x24+20+20 -e "docker attach enigma_contract_1" &
-xterm -geometry 100x24+600+20 -e "docker attach enigma_core_1" &
+xterm -geometry 120x20+600+20 -e "docker attach enigma_core_1" &
 echo 'Waiting for terminals to spawn...'
 sleep 10
 echo "Deploying contracts on testnet..."
 docker-compose exec contract bash -c "rm -rf ~/build/contracts/*"
 docker-compose exec contract bash -c "~/darq-truffle migrate --reset --network ganache"
 echo "Starting Surface..."
-xterm -geometry 100x24+600+380 -e  "docker-compose exec surface bash -c ./wait_launch.bash"  &
+xterm -geometry 120x20+600+330 \
+	  -e  "docker-compose exec surface bash -c ./wait_launch.bash"  &
+
+# When everything is ready, trigger a computation by running:
+# docker-compose exec contract bash -c "node integration/coin-mixer.js --with-register"
