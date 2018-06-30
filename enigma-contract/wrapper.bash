21e5bdf068e0b77a282fdd8a62d4bf4ea6049c04
Victor Grau Serrat <lacabra@users.noreply.github.com>
2018-06-30 00:26:39 -0500

    BUG: adjusting boot sequence

--- enigma-contract/wrapper.bash
+++ enigma-contract/wrapper.bash
@@ -2,5 +2,7 @@
 export CONTRACT_IP=`/sbin/ifconfig eth0 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
 echo "My IP address is " $CONTRACT_IP
 sed -i'' -e "s/127.0.0.1\:8545/$CONTRACT_IP\:8545/g" ~/enigma-contract/integration/coin-mixer.js 
+sed -i'' -e "s/localhost\:8545/$CONTRACT_IP\:8545/g" ~/enigma-contract/integration/coin-mixer.js
 sed -i'' -e "s/127.0.0.1/$CONTRACT_IP/g" ~/enigma-contract/truffle.js
+sed -i'' -e "s/localhost/$CONTRACT_IP/g" ~/enigma-contract/truffle.js
 cd ~/enigma-contract && ~/ganache-cli -h $CONTRACT_IP
--- launch_network_terminals.bash
+++ launch_network_terminals.bash
@@ -4,7 +4,7 @@
 # we capture the output on the current terminal & we can see the list of 
 # accounts that ganache creates (different every time)
 docker-compose up &
-xterm -geometry 90x24+20+20 -e "docker attach enigma_contract_1" &
+xterm -geometry 90x20+20+20 -e "docker attach enigma_contract_1" &
 xterm -geometry 120x20+600+20 -e "docker attach enigma_core_1" &
 echo 'Waiting for terminals to spawn...'
 sleep 10
@@ -12,9 +12,9 @@ echo "Deploying contracts on testnet..."
 docker-compose exec contract bash -c "rm -rf ~/enigma-contract/build/contracts/*"
 docker-compose exec contract bash -c "cd enigma-contract && ~/darq-truffle migrate --reset --network ganache"
 echo "Starting coin-mixer app..."
-docker-compose exec contract bash -c "node integration/coin-mixer.js"
+xterm -geometry 150x20+20+330 -e 'docker-compose exec contract bash -c "cd enigma-contract && node integration/coin-mixer.js; bash"' &
 echo "Starting Surface..."
-xterm -geometry 120x20+600+330 \
+xterm -geometry 120x20+600+450 \
 	  -e  "docker-compose exec surface bash -c ./wait_launch.bash"  &
 
 # When everything is ready, trigger a computation by running from another terminal:
