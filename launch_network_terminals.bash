61250b5a7306f05f20a9e8560a1f64a3e33b3616
Victor Grau Serrat <lacabra@users.noreply.github.com>
2018-07-10 09:52:07 -0500

    MAINT: added descriptive xterm titles

--- launch_network_terminals.bash
+++ launch_network_terminals.bash
@@ -4,15 +4,15 @@
 # we capture the output on the current terminal & we can see the list of 
 # accounts that ganache creates (different every time)
 docker-compose up &
-xterm -geometry 90x20+20+20 -e "docker attach enigma_contract_1" &
-xterm -geometry 120x20+600+20 -e "docker attach enigma_core_1" &
+xterm -T 'Enigma Contract' -geometry 90x20+20+20 -e "docker attach enigma_contract_1" &
+xterm -T 'Enigma Core' -geometry 120x20+600+20 -e "docker attach enigma_core_1" &
 echo 'Waiting for terminals to spawn...'
 sleep 10
 echo "Deploying contracts on testnet..."
 docker-compose exec contract bash -c "rm -rf ~/enigma-contract/build/contracts/*"
 docker-compose exec contract bash -c "cd enigma-contract && ~/darq-truffle migrate --reset --network ganache"
 echo "Starting coin-mixer app..."
-xterm -geometry 152x20+20+330 -e 'docker-compose exec contract bash -c "cd enigma-contract && node integration/coin-mixer.js; bash"' &
+xterm -T 'Enigma DApp' -geometry 152x20+20+330 -e 'docker-compose exec contract bash -c "cd enigma-contract && node integration/coin-mixer.js; bash"' &
 echo "Starting Surface..."
-xterm -geometry 120x20+900+80 \
+xterm -T 'Enigma Surface' -geometry 120x20+900+80 \
 	  -e  "docker-compose exec surface bash -c ./wait_launch.bash"  &
