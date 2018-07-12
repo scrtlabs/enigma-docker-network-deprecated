68172de9e9553bfdfeeca7f7b44bfe5bdff9e2eb
moria <moria@enigma.co>
2018-07-12 15:37:10 +0300

    FIX: core and contract terminals are attached too late to the containers; it is noticeable when the containers do not exist in advance

--- launch_network_terminals.bash
+++ launch_network_terminals.bash
@@ -4,10 +4,10 @@
 # we capture the output on the current terminal & we can see the list of 
 # accounts that ganache creates (different every time)
 docker-compose up &
-xterm -T 'Enigma Contract' -geometry 90x20+20+20 -e "docker attach enigma_contract_1" &
-xterm -T 'Enigma Core' -geometry 120x20+600+20 -e "docker attach enigma_core_1" &
 echo 'Waiting for terminals to spawn...'
 sleep 10
+xterm -T 'Enigma Contract' -geometry 90x20+20+20 -e "docker attach enigma_contract_1" &
+xterm -T 'Enigma Core' -geometry 120x20+600+20 -e "docker attach enigma_core_1" &
 echo "Deploying contracts on testnet..."
 docker-compose exec contract bash -c "rm -rf ~/enigma-contract/build/contracts/*"
 docker-compose exec contract bash -c "cd enigma-contract && ~/darq-truffle migrate --reset --network ganache"
