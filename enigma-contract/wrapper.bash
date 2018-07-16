b7bc662c85e7ad5e747135c524b1524db35a8273
Victor Grau Serrat <lacabra@users.noreply.github.com>
2018-07-16 16:42:45 +0200

    BUG: fix typo in sript

--- enigma-contract/wrapper.bash
+++ enigma-contract/wrapper.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CONTRACT_IP=`/sbin/ifconfig eth0 | awk '/inet addr/ {gsub("addr:", "", $2); print $2}'`
 echo "My IP address is " $CONTRACT_IP
-sed -i '' -e ':a;N;$!ba;' -e "s/ganache: {[^}]*}/ganache: { host: \"$CONTRACT_IP\", port: 8545, network_id: \"*\"}/" ~/enigma-contract/truffle.js
+sed -i'' -e ':a;N;$!ba;' -e "s/ganache: {[^}]*}/ganache: { host: \"$CONTRACT_IP\", port: 8545, network_id: \"*\"}/" ~/enigma-contract/truffle.js
 cd ~/enigma-contract && ~/ganache-cli -h $CONTRACT_IP
