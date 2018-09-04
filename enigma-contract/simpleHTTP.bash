8d1178a15cceaa35ca23e3096fb811d4384d99e0
Victor Grau Serrat <lacabra@users.noreply.github.com>
2018-09-04 15:52:45 -0600

    BLD: ntw publishes enigma token address

--- enigma-contract/simpleHTTP.bash
+++
@@ -1,3 +0,0 @@
-while true; do 
-	echo -ne "HTTP/1.1 200 OK\r\nContent-Length:$(wc -c < ~/enigma-contract/enigmacontract.txt)\r\n\r\n$(cat ~/enigma-contract/enigmacontract.txt)" | nc -l -p 8081
-done
